2efa9921d54082abc370ba95b6082ecfebd108e0
Alejandro Galue <agalue@opennms.org>
2016-07-22 16:01:30 -0400

    Simplifying karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -4,16 +4,6 @@ webpackConfig.output = {};
 
 module.exports = function (config) {
   config.set({
-
-    plugins: [
-      require('karma-webpack'),
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
-      require('karma-ng-html2js-preprocessor'),
-      require('karma-ng-scenario')
-    ],
-
     basePath: __dirname,
 
     frameworks: [
